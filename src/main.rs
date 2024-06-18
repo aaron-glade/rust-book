326db806e1e79e34d48cd01f5b0009f2bdfe3731
Aaron Lee <aaron@gladetool.com>
2024-06-18 14:36:38 -0700

    first rust vector and hashmap exercise

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::io;
+use std::collections::HashMap;
 use a::b;
 pub mod a;
 
 fn main() {
+    print_vec_med_and_mode();
     print_fib();
     play_fahrenheit_game();
     print_rectangle_area();
@@ -91,4 +93,51 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         other.height <= self.height && other.width <= self.width
     }
+}
+
+fn vec_median_and_mode(a: &[i32]) -> (f64, i32) {
+    let mut vec = Vec::from(a);
+    vec.sort();
+    let size = vec.len();
+    let median: f64;
+
+    if size == 0 {
+        median = 0.0;
+    } else if size % 2 == 0 {
+        let m1 = vec[size / 2 - 1] as f64;
+        let m2 = vec[size / 2] as f64;
+        median = (m1 + m2) / 2.0;
+    } else {
+        median = vec[size / 2] as f64;
+    }
+
+    let mut counts: HashMap<i32, usize> = HashMap::new();
+
+    for num in vec {
+        let count = counts.entry(num).or_insert(0);
+        *count += 1;
+    }
+    let mut mode: i32 = 0;
+    let mut mode_size: usize = 0;
+
+    for (num, size) in counts.into_iter() {
+        if mode_size < size {
+            mode_size = size;
+            mode = num;
+        }
+    }
+
+    
+    return (median, mode);
+}
+
+fn print_vec_med_and_mode() {
+
+    let a = [1, 1, 1, 3, 4, 56, 6, 234, 123];
+    let b = [1, 23, 3, 10, 10, 2];
+
+    let c = vec_median_and_mode(&a);
+    let d = vec_median_and_mode(&b);
+
+    println!("{c:?} and {d:?}");
 }
