b0a6f553c621d019dabc1b32ed300e04244ceb3b
Aaron Lee <aaron@gladetool.com>
2024-06-20 11:11:45 -0700

    errs to stderr

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -8,14 +8,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     match minigrep::run(config) {
         Ok(()) => {"minigrep ran successfully";},
         Err(e) => {
-            println!("application error: {e}");
+            eprintln!("application error: {e}");
             process::exit(1);
         }
     }
